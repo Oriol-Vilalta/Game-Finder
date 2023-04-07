283b1ddb59b4c61af342bd97406ead1887975ece
Ivan-PS <51946898+Ivan-PS@users.noreply.github.com>
2023-04-07 21:22:58 +0200

    Delete gameFinder directory

--- gameFinder/game_Finder/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from game_Finder.models import *
-
-# Register your models here.
-admin.site.register(Game)
-admin.site.register(Genre)
-admin.site.register(DevelopingCompany)
-admin.site.register(Platform)
