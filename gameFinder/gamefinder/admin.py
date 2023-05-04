8619de593fbf03e9e9a85b9b64dc4a7b76877348
MariaPlanella02 <73690620+MariaPlanella02@users.noreply.github.com>
2023-05-04 20:20:41 +0200

    Projecte arreglat

---
+++ gameFinder/gamefinder/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from gamefinder.models import *
+
+# Register your models here.
+admin.site.register(Game)
+admin.site.register(Genre)
+admin.site.register(DevelopingCompany)
+admin.site.register(Platform)
