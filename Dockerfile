8fda2952fc7d46906da6e50a154c7c9f107d3355
Oriol Vilalta <orivila48@gmail.com>
2023-05-04 20:14:01 +0200

    Restart project

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3.9
-ENV PYTHONDONTWRITEBYTECODE 1
-WORKDIR /gameFinder
-COPY Pipfile Pipfile.lock /gameFinder/
-RUN pip install pipenv && pipenv install --system
-COPY . /gameFinder/
--- gameFinder/gamefinder/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from gamefinder.models import *
-
-# Register your models here.
-admin.site.register(Game)
-admin.site.register(Genre)
-admin.site.register(DevelopingCompany)
-admin.site.register(Platform)
